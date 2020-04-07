8f3449347aee29892fc4ea39bba0f5a425d34c07
sapphon <sapphon@gmail.com>
2020-04-07 19:54:15 -0400

    People now move randomly [https://trello.com/c/1zzPA9U1]

--- Assets/Scripts/Simulation/Model/Person.cs
+++ Assets/Scripts/Simulation/Model/Person.cs
@@ -1,18 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Person : MonoBehaviour
 {
+    private Vector3 velocity;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        chooseMovementVector();
+    }
+
+    void chooseMovementVector()
+    {
+        this.velocity = new Vector3(UnityEngine.Random.Range(-1f, 1f), 0, UnityEngine.Random.Range(-1f, 1f)).normalized;
+    }
+
+    
+    void move()
+    {
+        this.gameObject.transform.Translate(velocity * Time.deltaTime);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        move();
     }
 }
