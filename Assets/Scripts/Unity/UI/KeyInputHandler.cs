6a60355a480b732c0999c8a1ddcd71c82ca2a80c
sapphon <sapphon@gmail.com>
2020-05-16 01:39:49 -0400

    L-ALT now changes cursor modes between camera use and parameter adjustment [https://trello.com/c/6mxe9gBl]
    Also re-anchored dashboard to fit smaller resolutions

--- Assets/Scripts/Unity/UI/KeyInputHandler.cs
+++ Assets/Scripts/Unity/UI/KeyInputHandler.cs
@@ -4,6 +4,14 @@ using UnityEngine.SceneManagement;
 
 public class KeyInputHandler : MonoBehaviour
 {
+    private MSCameraController cameraControls;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        this.cameraControls = GameObject.FindObjectOfType<MSCameraController>();
+    }
+
     private void Update()
     {
         if (Input.GetKeyUp(KeyCode.Escape))
@@ -14,9 +22,25 @@ public class KeyInputHandler : MonoBehaviour
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
+        else if (Input.GetKeyUp(KeyCode.LeftAlt))
+        {
+            this.toggleCursor();
+        }
     }
-    
-    
+
+    void toggleCursor()
+    {
+        if (Cursor.lockState == CursorLockMode.Locked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            this.cameraControls.enabled = false;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            this.cameraControls.enabled = true;
+        }
+    }
 
     void quit(){
 #if UNITY_EDITOR
